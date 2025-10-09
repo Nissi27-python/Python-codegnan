b6f2b52ac1e60efd7cd82e3bae246856bfe58c9b
Nissi27-python <swarnanissi360@gmail.com>
2025-10-09 15:58:30 +0530

    Update for loop.py

--- for loop.py
+++ for loop.py
@@ -20,4 +20,33 @@ for i in range(len(lst)):
     if lst[i] % 2 == 0:
         res.append(lst[i])
 print(res)
+#tasks
+#find min element in list using for loop
+#find max element in list using for loop
+#find sum of elements in the list
+#find the product of a list
+
+#max element in list using for loop
+lst=list(map(int,input().split()))
+max_num=lst[0]
+for i in range(len(lst)):
+    if lst[i] > max_num:    
+        max_num = lst[i]     
+print("Max number is:", max_num)
+
+#sum of elements in the list
+lst=list(map(int,input().split()))
+sum = 0
+for num in lst:
+    sum = sum + num
+    print("sum of numbers: ",sum)
+
+#product of elements in the list
+lst=list(map(int,input().split()))
+prod = 1
+for num in lst:
+    prod = prod * num
+    print("product of numbers: ",prod)
+
     
+
